e230742de8791075f08221706d466c102d1530a3
Vivek Sharma <vivek.sharma@mpokket.com>
2024-10-20 13:11:14 +0530

    LoginInputStyle added

--- site/src/jsMain/kotlin/com/hypheno/blog/pages/admin/Login.kt
+++ site/src/jsMain/kotlin/com/hypheno/blog/pages/admin/Login.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.hypheno.blog.models.Theme
+import com.hypheno.blog.styles.LoginInputStyle
 import com.hypheno.blog.util.Constants.FONT_FAMILY
 import com.hypheno.blog.util.Res
 import com.varabyte.kobweb.compose.css.FontWeight
@@ -33,6 +34,7 @@ import com.varabyte.kobweb.compose.ui.modifiers.width
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.components.text.SpanText
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.LineStyle
@@ -64,7 +66,7 @@ fun LoginScreen(modifier: Modifier = Modifier) {
             )
             Input(
                 type = InputType.Text,
-                attrs = Modifier
+                attrs = LoginInputStyle.toModifier()
                     .margin(bottom = 12.px)
                     .width(350.px)
                     .height(54.px)
@@ -72,11 +74,6 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                     .background(Colors.White)
                     .fontFamily(FONT_FAMILY)
                     .fontSize(14.px)
-                    .border(
-                        width = 0.px,
-                        style = LineStyle.None,
-                        color = Colors.Transparent
-                    )
                     .outline(
                         width = 0.px,
                         style = LineStyle.None,
@@ -88,7 +85,7 @@ fun LoginScreen(modifier: Modifier = Modifier) {
             )
             Input(
                 type = InputType.Password,
-                attrs = Modifier
+                attrs = LoginInputStyle.toModifier()
                     .margin(bottom = 12.px)
                     .width(350.px)
                     .height(54.px)
@@ -96,11 +93,6 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                     .background(Colors.White)
                     .fontFamily(FONT_FAMILY)
                     .fontSize(14.px)
-                    .border(
-                        width = 0.px,
-                        style = LineStyle.None,
-                        color = Colors.Transparent
-                    )
                     .outline(
                         width = 0.px,
                         style = LineStyle.None,
