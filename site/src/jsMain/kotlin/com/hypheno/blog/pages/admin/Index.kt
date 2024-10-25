58e2816f31b931fec33e4172f37ce74d7788447b
Vivek Sharma <vivek.sharma@mpokket.com>
2024-10-25 11:33:24 +0530

    SidePanel added

--- site/src/jsMain/kotlin/com/hypheno/blog/pages/admin/Index.kt
+++ site/src/jsMain/kotlin/com/hypheno/blog/pages/admin/Index.kt
@@ -1,14 +1,39 @@
 package com.hypheno.blog.pages.admin
 
 import androidx.compose.runtime.Composable
+import com.hypheno.blog.components.SidePanel
+import com.hypheno.blog.util.Constants.PAGE_WIDTH
 import com.hypheno.blog.util.IsUserLoggedIn
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
 import com.varabyte.kobweb.core.Page
+import org.jetbrains.compose.web.css.px
 
 @Page
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
     IsUserLoggedIn {
+        HomePage()
+    }
+}
 
+@Composable
+fun HomePage() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .maxWidth(PAGE_WIDTH.px)
+        ) {
+            SidePanel()
+        }
     }
 }
