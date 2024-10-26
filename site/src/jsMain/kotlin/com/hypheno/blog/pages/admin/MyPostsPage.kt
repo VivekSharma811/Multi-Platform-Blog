3bb1a92510d886ba58c8eb9a06f4b4ea5b6653a0
Vivek Sharma <vivek.sharma@mpokket.com>
2024-10-26 19:08:58 +0530

    UI optimised

--- site/src/jsMain/kotlin/com/hypheno/blog/pages/admin/MyPostsPage.kt
+++ site/src/jsMain/kotlin/com/hypheno/blog/pages/admin/MyPostsPage.kt
@@ -71,7 +71,7 @@ fun MyPostsScreen() {
         fetchMyPosts(
             skip = 0,
             onSuccess = {
-                if(it is ApiListResponse.Success) {
+                if (it is ApiListResponse.Success) {
                     myPosts.addAll(it.data)
                 }
             },
@@ -103,9 +103,12 @@ fun MyPostsScreen() {
             }
             Row(
                 modifier = Modifier
-                    .fillMaxWidth(90.percent)
+                    .fillMaxWidth(
+                        if (breakpoint > Breakpoint.MD) 80.percent
+                        else 90.percent
+                    )
                     .margin(bottom = 24.px),
-                verticalAlignment = Alignment.CenterVertically,
+                verticalAlignment = Alignment.Bottom,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
@@ -122,6 +125,7 @@ fun MyPostsScreen() {
                 }
                 Button(
                     attrs = Modifier
+                        .margin(right = 20.px)
                         .height(54.px)
                         .padding(leftRight = 24.px)
                         .backgroundColor(Theme.Red.rgb)
@@ -138,7 +142,10 @@ fun MyPostsScreen() {
                     SpanText(text = "Delete")
                 }
             }
-            Posts(posts = myPosts)
+            Posts(
+                breakpoint = breakpoint,
+                posts = myPosts
+            )
         }
     }
 }
