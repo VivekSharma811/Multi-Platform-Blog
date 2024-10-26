282278087a4cb6dae556fb82a74057fc57c52cf6
Vivek Sharma <vivek.sharma@mpokket.com>
2024-10-26 18:02:06 +0530

    SearchBar added

--- site/src/jsMain/kotlin/com/hypheno/blog/pages/admin/MyPostsPage.kt
+++ site/src/jsMain/kotlin/com/hypheno/blog/pages/admin/MyPostsPage.kt
@@ -2,16 +2,25 @@ package com.hypheno.blog.pages.admin
 
 import androidx.compose.runtime.Composable
 import com.hypheno.blog.components.AdminPageLayout
+import com.hypheno.blog.components.SearchBar
 import com.hypheno.blog.components.SidePanel
 import com.hypheno.blog.util.Constants.PAGE_WIDTH
+import com.hypheno.blog.util.Constants.SIDE_PANEL_WIDTH
 import com.hypheno.blog.util.IsUserLoggedIn
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.margin
 import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
 @Page("myposts")
@@ -24,7 +33,27 @@ fun MyPostsPage() {
 
 @Composable
 fun MyPostsScreen() {
-    AdminPageLayout {
+    val breakpoint = rememberBreakpoint()
 
+    AdminPageLayout {
+        Column(
+            modifier = Modifier
+                .margin(topBottom = 50.px)
+                .fillMaxSize()
+                .padding(left = if (breakpoint > Breakpoint.MD) SIDE_PANEL_WIDTH.px else 0.px),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(
+                        if (breakpoint > Breakpoint.MD) 30.percent
+                        else 50.percent
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                SearchBar(onEnterClick = {})
+            }
+        }
     }
 }
