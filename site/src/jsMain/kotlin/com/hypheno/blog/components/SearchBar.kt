89297aedcfa46bfa65ed068cbc2f7f9accc51f00
Vivek Sharma <vivek.sharma@mpokket.com>
2024-10-26 23:17:22 +0530

    SearchBar visibility updated

--- site/src/jsMain/kotlin/com/hypheno/blog/components/SearchBar.kt
+++ site/src/jsMain/kotlin/com/hypheno/blog/components/SearchBar.kt
@@ -17,6 +17,7 @@ import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
 import com.varabyte.kobweb.compose.ui.modifiers.border
 import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
 import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.height
 import com.varabyte.kobweb.compose.ui.modifiers.id
@@ -36,10 +37,13 @@ import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Input
 
 @Composable
-fun SearchBar(onEnterClick: () -> Unit) {
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    onEnterClick: () -> Unit
+) {
     var focused by remember { mutableStateOf(false) }
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .padding(left = 20.px)
             .height(54.px)
@@ -57,21 +61,26 @@ fun SearchBar(onEnterClick: () -> Unit) {
             modifier = Modifier
                 .color(Theme.DarkGray.rgb)
                 .margin(right = 14.px)
-                .transition(CSSTransition(property = "color", duration = 200.ms)),
+                .transition(
+                    CSSTransition(
+                        property = "color",
+                        duration = 200.ms
+                    )
+                ),
             size = IconSize.SM
         )
         Input(
             type = InputType.Text,
             attrs = Modifier
                 .id(Id.adminSearchBar)
-                .fillMaxWidth()
+                .fillMaxSize()
                 .color(Colors.Black)
                 .backgroundColor(Colors.Transparent)
                 .noBorder()
                 .onFocusIn { focused = true }
                 .onFocusOut { focused = false }
                 .onKeyDown {
-                    if(it.key == "Enter") {
+                    if (it.key == "Enter") {
                         onEnterClick()
                     }
                 }
