458b1622ed34518b6b306e97a618fee682d63f2e
Vivek Sharma <vivek.sharma@mpokket.com>
2024-10-26 22:20:34 +0530

    CategoryChip added

---
+++ site/src/jsMain/kotlin/com/hypheno/blog/components/CategoryChip.kt
@@ -0,0 +1,43 @@
+package com.hypheno.blog.components
+
+import androidx.compose.runtime.Composable
+import com.hypheno.blog.models.Category
+import com.hypheno.blog.models.Theme
+import com.hypheno.blog.util.Constants.FONT_FAMILY
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.border
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.silk.components.text.SpanText
+import org.jetbrains.compose.web.css.LineStyle
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun CategoryChip(category: Category) {
+    Box(
+        modifier = Modifier
+            .height(32.px)
+            .padding(leftRight = 14.px)
+            .borderRadius(r = 100.px)
+            .border(
+                width = 1.px,
+                style = LineStyle.Solid,
+                color = Theme.HalfBlack.rgb
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        SpanText(
+            modifier = Modifier
+                .fontFamily(FONT_FAMILY)
+                .fontSize(12.px)
+                .color(Theme.HalfBlack.rgb),
+            text = category.name
+        )
+    }
+}
