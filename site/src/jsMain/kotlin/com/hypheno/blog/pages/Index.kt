1afe2d3e78c78829c86b8dbb9db127f335a853df
Vivek Sharma <vivek.sharma@mpokket.com>
2024-11-02 14:02:59 +0530

    HeaderSection added

--- site/src/jsMain/kotlin/com/hypheno/blog/pages/Index.kt
+++ site/src/jsMain/kotlin/com/hypheno/blog/pages/Index.kt
@@ -1,18 +1,26 @@
 package com.hypheno.blog.pages
 
 import androidx.compose.runtime.*
+import com.hypheno.blog.sections.HeaderSection
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 fun HomePage() {
-    // TODO: Replace the following with your own content
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    val breakpoint = rememberBreakpoint()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HeaderSection(breakpoint = breakpoint)
     }
 }
---
+++ site/src/jsMain/kotlin/com/hypheno/blog/sections/HeaderSection.kt
@@ -0,0 +1,93 @@
+package com.hypheno.blog.sections
+
+import androidx.compose.runtime.Composable
+import com.hypheno.blog.components.SearchBar
+import com.hypheno.blog.models.Category
+import com.hypheno.blog.models.Theme
+import com.hypheno.blog.styles.CategoryItemStyle
+import com.hypheno.blog.util.Constants.FONT_FAMILY
+import com.hypheno.blog.util.Constants.HEADER_HEIGHT
+import com.hypheno.blog.util.Constants.PAGE_WIDTH
+import com.hypheno.blog.util.Res
+import com.varabyte.kobweb.compose.css.Cursor
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextDecorationLine
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.foundation.layout.Spacer
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.cursor
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
+import com.varabyte.kobweb.compose.ui.modifiers.textDecorationLine
+import com.varabyte.kobweb.compose.ui.modifiers.width
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun HeaderSection(breakpoint: Breakpoint) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .backgroundColor(Theme.Secondary.rgb),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .backgroundColor(Theme.Secondary.rgb)
+                .maxWidth(PAGE_WIDTH.px),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Header(breakpoint = breakpoint)
+        }
+    }
+}
+@Composable
+fun Header(breakpoint: Breakpoint) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(if (breakpoint > Breakpoint.MD) 80.percent else 90.percent)
+            .height(HEADER_HEIGHT.px),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .margin(right = 50.px)
+                .width(if (breakpoint >= Breakpoint.SM) 100.px else 70.px)
+                .cursor(Cursor.Pointer)
+                .onClick { },
+            src = Res.Image.logoHome,
+            description = "Logo Image"
+        )
+        if (breakpoint >= Breakpoint.LG) {
+            Category.entries.forEach { category ->
+                Link(
+                    modifier = CategoryItemStyle.toModifier()
+                        .margin(right = 24.px)
+                        .fontFamily(FONT_FAMILY)
+                        .fontSize(16.px)
+                        .fontWeight(FontWeight.Medium)
+                        .textDecorationLine(TextDecorationLine.None)
+                        .onClick { },
+                    path = "",
+                    text = category.name
+                )
+            }
+        }
+        Spacer()
+        SearchBar(onEnterClick = {})
+    }
+}
